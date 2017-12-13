f0b8825a44fec0ce37cbfb37d688fedde787b2c0
rizky Kharisma <ngeng.ngengs@gmail.com>
2017-12-13 15:24:00 +0700

    Add help type config to garage activity
    Signed-off-by: rizky Kharisma <ngeng.ngengs@gmail.com>

--- app/src/main/java/com/ngengs/skripsi/todongban/MainActivityGarage.java
+++ app/src/main/java/com/ngengs/skripsi/todongban/MainActivityGarage.java
@@ -41,6 +41,9 @@ public class MainActivityGarage extends AppCompatActivity {
         mNavView = findViewById(R.id.nav_view);
         mNavView.setNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
+                case R.id.menu_config_help:
+                    HelpSettingActivity.runSetting(this);
+                    break;
                 case R.id.menu_signout:
                     SignoutActivity.runSignout(this);
                     break;
